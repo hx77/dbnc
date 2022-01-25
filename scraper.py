ee60fa9f7aa882d2aefe48a7f737b74ba9654cf5
hx77 <55721409+hx77@users.noreply.github.com>
2022-01-25 01:00:06 -0500

    create add_new_col()

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 # Change the file name to the input file name
-file_name = "Python Quiz Input.csv"
+file_name = "Python Quiz Input - Sheet1.csv"
 usps_url = "https://tools.usps.com/tools/app/ziplookup/zipByAddress"
 
 
@@ -14,7 +14,7 @@ def send_request(url: str, payload: dict):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/97.0.4692.71 Safari/537.36 '
     }
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, data=payload)
     return response.json()
 
 
@@ -28,13 +28,18 @@ def create_validity_list(result_status: str, validity_list: list):
         validity_list.append('Invalid')
 
 
+def add_new_col(df, data_list: list, header: str):
+    new_col = pd.DataFrame(data_list, columns=[header])
+    df[header] = new_col[header]
+
+
 def read_input_data():
     """
 
     :return:
     """
-    # Read CSV file into DataFrame df
-    df = pd.read_csv(file_name)
+    # Read CSV file into DataFrame df, the first row is header
+    df = pd.read_csv(file_name, header=0, index_col=None)
     validity_list = []
 
     for i in range(len(df)):
@@ -50,9 +55,9 @@ def read_input_data():
         response = send_request(usps_url, payload)
         create_validity_list(response['resultStatus'], validity_list)
 
-    new_col = pd.DataFrame(validity_list, columns=['Validity'])
-    df['Validity'] = new_col['Validity']
-    print(df)
+    add_new_col(df, validity_list, 'Validity')
+
+    df.to_csv(file_name, index=False)
 
 
 def main():
