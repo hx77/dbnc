2d796f0e878d5ad47ff9aa06f1c46863e082b227
hx77 <55721409+hx77@users.noreply.github.com>
2022-01-24 23:52:44 -0500

    Create a row object for each row

--- scraper.py
+++ scraper.py
@@ -1,5 +1,20 @@
+import pandas as pd
+
+file_name = "Python Quiz Input.csv"
+
+
+def read_input_data():
+    # Read CSV file into DataFrame df
+    df = pd.read_csv(file_name)
+
+    for i in range(len(df)):
+        # Create a row object for each row
+        row_obj = df.loc[i]
+        print(row_obj)
+
+
 def main():
-    print('valid')
+    read_input_data()
 
 
 if __name__ == '__main__':
