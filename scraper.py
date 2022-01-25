c87870ea143c5e1eafa76e6c6127d9552188c2d6
hx77 <55721409+hx77@users.noreply.github.com>
2022-01-25 01:37:17 -0500

    add comments

--- scraper.py
+++ scraper.py
@@ -2,22 +2,26 @@ import pandas as pd
 import requests
 import sys
 
-usps_url = "https://tools.usps.com/tools/app/ziplookup/zipByAddress"
 
-
-def send_request(url: str, payload: dict):
+def send_request(payload: dict):
     """
     Send a post request to the USPS API and return JSON object of the result
     """
+    url = "https://tools.usps.com/tools/app/ziplookup/zipByAddress"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/97.0.4692.71 Safari/537.36 '
     }
-    response = requests.post(url, headers=headers, data=payload)
+    try:
+        response = requests.post(url, headers=headers, data=payload)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
     return response.json()
 
 
-def create_validity_list(result_status: str, validity_list: list):
+def append_validity_list(result_status: str, validity_list: list):
     """
     Create validity list depending on resultStatus in response
     """
@@ -28,17 +32,21 @@ def create_validity_list(result_status: str, validity_list: list):
 
 
 def add_new_col(df, data_list: list, header: str):
+    """
+    Add a new column using data in data_list and header
+    """
     new_col = pd.DataFrame(data_list, columns=[header])
     df[header] = new_col[header]
 
 
 def read_input_data(file_name):
     """
-
-    :return:
+    Read in input file and write the output to the same file
     """
+
     # Read CSV file into DataFrame df, the first row is header
     df = pd.read_csv(file_name, header=0, index_col=None)
+    # Initialize a validity_list to save the result
     validity_list = []
 
     for i in range(len(df)):
@@ -51,16 +59,15 @@ def read_input_data(file_name):
             'state': row_obj.St,
             'zip': row_obj.ZIPCode
         }
-        response = send_request(usps_url, payload)
-        create_validity_list(response['resultStatus'], validity_list)
+        response = send_request(payload)
+        append_validity_list(response['resultStatus'], validity_list)
 
     add_new_col(df, validity_list, 'Validity')
-
+    # Save DataFrame to the original csv file
     df.to_csv(file_name, index=False)
 
 
 def main():
-    print(len(sys.argv))
     if len(sys.argv) != 2:
         raise ValueError('Please provide file name.')
 
